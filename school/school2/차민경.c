aac35b1409e2ad1969302bf48df96b12c7e1ec96
chamingyeong <mingyeong1104@naver.com>
2026-03-25 11:21:50 +0900

    second solution

--- school/school2/차민경.c
+++ school/school2/차민경.c
@@ -1,12 +1,11 @@
 #include <stdio.h>
-#include <unistd.h>
 
 extern void get_school_position(int school_index, int* posX, int* posY);
 extern void get_student_position(int student_index, int* posX, int* posY);
 extern void set_student_school(int student_index, int school_index);
 static int total = 0;
 
-typedef struct stduent {
+typedef struct student {
     int index;
     int posX;
     int posY;
@@ -22,8 +21,8 @@ int get_distant(int student_posX, int student_posY, int school_posX, int school_
     return dx * dx + dy * dy;
 }
 
-int check_school_max(int min_school_index, int school_count[3]) {
-    if (school_count[min_school_index] >= 3500)
+int check_school_max(int school_index, int school_count[3]) {
+    if (school_count[school_index] >= 3500)
         return 0;
     return 1;
 }
@@ -44,16 +43,14 @@ void swap(t_student *student1, t_student *student2)
     *student2 = temp;
 }
 
-void select_sort(t_student student[10000])
+void sort_by_diff(t_student student[10000])
 {
-    int MAX = 0;
     for (int i = 0; i < 9999; i++)
     {
         for (int j = i+1; j < 10000; j++)
         {
             if (student[i].diff[0] < student[j].diff[0] ||
-            (student[i].diff[0] == student[j].diff[0] &&
-                student[i].diff[1] < student[j].diff[1]))
+            (student[i].diff[0] == student[j].diff[0] && student[i].diff[1] < student[j].diff[1]))
             {
                 swap(&student[i], &student[j]);
             }
@@ -63,25 +60,25 @@ void select_sort(t_student student[10000])
 
 int choose_school(t_student *student, int school_count[3])
 {
-    int school;
+    int school_index;
 
-    school = student->order[0];
-    if (check_school_max(school, school_count))
+    school_index = student->order[0];
+    if (check_school_max(school_index, school_count))
     {
-        school_count[school]++;
-        return school;
+        school_count[school_index]++;
+        return school_index;
     }
 
-    school = student->order[1];
-    if (check_school_max(school, school_count))
+    school_index = student->order[1];
+    if (check_school_max(school_index, school_count))
     {
-        school_count[school]++;
-        return school;
+        school_count[school_index]++;
+        return school_index;
     }
 
-    school = student->order[2];
-    school_count[school]++;
-    return school;
+    school_index = student->order[2];
+    school_count[school_index]++;
+    return school_index;
 }
 
 void set_order(t_student *student)
@@ -117,7 +114,7 @@ void run_contest(void)
     int school_posX[3], school_posY[3];
     t_student student[10000];
     int school_count[3] = { 0, 0, 0 };
-    int min_school_index = 0;
+    int assigned_school = 0;
     total = 0;
 
     for (int school_index = 0; school_index < 3; school_index++)
@@ -125,25 +122,33 @@ void run_contest(void)
         get_school_position(school_index, &school_posX[school_index], &school_posY[school_index]);
     }
 
-
-    for (int people_index = 0; people_index < 10000; people_index++)
+    for (int student_index = 0; student_index < 10000; student_index++)
     {
-        student[people_index].index = people_index;
-        get_student_position(people_index, &student[people_index].posX, &student[people_index].posY);
+        student[student_index].index = student_index;
+        get_student_position(student_index, &student[student_index].posX, &student[student_index].posY);
         for (int school_index = 0; school_index < 3; school_index++)
         {
-            student[people_index].distant[school_index] = get_distant(student[people_index].posX,student[people_index].posY, school_posX[school_index], school_posY[school_index]);
+            student[student_index].distant[school_index] = get_distant(
+            student[student_index].posX,
+            student[student_index].posY,
+            school_posX[school_index],
+            school_posY[school_index]
+            );
         }
-        set_order(&student[people_index]);
-        set_diff(&student[people_index]);
-
+        set_order(&student[student_index]);
+        set_diff(&student[student_index]);
     }
-    select_sort(student);
-    choose_school(student, school_count);
+    sort_by_diff(student);
+    for (int student_index = 0; student_index < 10000; student_index++)
+    {
+        assigned_school = choose_school(&student[student_index], school_count);
+        total += student[student_index].distant[assigned_school];
+        set_student_school(student[student_index].index, assigned_school);
+    }
 
-    printf("total : %d\n", total);
-    printf("school 0 : %d\n", school_count[0]);
-    printf("school 1 : %d\n", school_count[1]);
-    printf("school 2 : %d\n", school_count[2]);
+//    printf("total : %d\n", total);
+//    printf("school 0 : %d\n", school_count[0]);
+//    printf("school 1 : %d\n", school_count[1]);
+//    printf("school 2 : %d\n", school_count[2]);
 
 }
